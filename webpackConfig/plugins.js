97fd865138accc222177df99f0128aa1f531ee6a
jianghaoran116 <jianghaoran@outlook.com>
2019-05-23 08:43:59 +0800

    htmlWebpackPlugin & cleanWebpackPlugin

--- webpackConfig/plugins.js
+++ webpackConfig/plugins.js
@@ -1,7 +1,15 @@
-const plugins = (env, argv) => {
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const plugins = (env, argv) => {
     return [
-        
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html')
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: [`${path.resolve(__dirname, '../')}dist*.*`]
+        })
     ]
 }
 module.exports = (env, argv) => {
