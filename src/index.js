b31392f154fb1cdf0e108b97edfb4915f9ddc44e
jianghaoran116 <jianghaoran@outlook.com>
2019-06-21 17:56:15 +0800

    二叉树

--- src/index.js
+++ src/index.js
@@ -82,17 +82,21 @@
 // console.log(linkedList.remove(15))
 // console.log(linkedList.toString())
 
-import { HashLinearProbing } from './helpers/dataStructure/HashLinearProbing'
-var hashLinearProbing = new HashLinearProbing();
-hashLinearProbing.put('Gandalf', 'gandalf@email.com');
-hashLinearProbing.put('John', 'johnsnow@email.com');
-hashLinearProbing.put('Tyrion', 'tyrion@email.com');
-hashLinearProbing.put('Aaron', 'aaron@email.com');
-hashLinearProbing.put('Donnie', 'donnie@email.com');
-hashLinearProbing.put('Ana', 'ana@email.com');
-hashLinearProbing.put('Jonathan', 'jonathan@email.com');
-hashLinearProbing.put('Jamie', 'jamie@email.com');
-hashLinearProbing.put('Sue', 'sue@email.com');
-hashLinearProbing.put('Mindy', 'mindy@email.com');
-hashLinearProbing.put('Paul', 'paul@email.com');
-hashLinearProbing.put('Nathan', 'nathan@email.com');
+// import { HashLinearProbing } from './helpers/dataStructure/HashLinearProbing'
+// var hashLinearProbing = new HashLinearProbing();
+// hashLinearProbing.put('Gandalf', 'gandalf@email.com');
+// hashLinearProbing.put('John', 'johnsnow@email.com');
+// hashLinearProbing.put('Tyrion', 'tyrion@email.com');
+// hashLinearProbing.put('Aaron', 'aaron@email.com');
+// hashLinearProbing.put('Donnie', 'donnie@email.com');
+// hashLinearProbing.put('Ana', 'ana@email.com');
+// hashLinearProbing.put('Jonathan', 'jonathan@email.com');
+// hashLinearProbing.put('Jamie', 'jamie@email.com');
+// hashLinearProbing.put('Sue', 'sue@email.com');
+// hashLinearProbing.put('Mindy', 'mindy@email.com');
+// hashLinearProbing.put('Paul', 'paul@email.com');
+// hashLinearProbing.put('Nathan', 'nathan@email.com');
+
+
+import { BinarySearchTree } from './helpers/dataStructure/BinarySearchTree'
+var binarySearchTree = new BinarySearchTree();
