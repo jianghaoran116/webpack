2ded4c9676d7bbcd64435ecbc52fba2e665271aa
haoran <jianghrh@yonyou.com>
2019-07-02 19:40:36 +0800

    code split

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+async function getComponent() {
+	const { default: _ } = await import(/* webpackChunkName:"lodash" */ 'lodash');
+	const element = document.createElement('div');
+	element.innerHTML = _.join(['Dell', 'Lee'], '-');
+	return element;
+}
 
-
-  function getComponent() {
-  	return import(/* webpackChunkName:"lodash" */ 'lodash').then(({ default: _ }) => {
-  		var element = document.createElement('div');
-  		element.innerHTML = _.join(['Dell', 'Lee'], '-');
-  		return element;
-  	})
-  }
-
-  getComponent().then(element => {
-  	document.body.appendChild(element);
-  });
+document.addEventListener('click', () =>{
+	getComponent().then(element => {
+		document.body.appendChild(element);
+	});
+})
