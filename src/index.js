b96934597e744b8ac3f48cac604164725cf0bf99
jianghaoran116 <jianghaoran@outlook.com>
2019-07-04 16:19:01 +0800

    webpack prefetch preload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-async function getComponent() {
-	const { default: _ } = await import(/* webpackChunkName:"lodash" */ 'lodash');
-	const element = document.createElement('div');
-	element.innerHTML = _.join(['Dell', 'Lee'], '-');
-	return element;
-}
+// async function getComponent() {
+// 	const { default: _ } = await import(/* webpackChunkName:"lodash" */ 'lodash');
+// 	const element = document.createElement('div');
+// 	element.innerHTML = _.join(['Dell', 'Lee'], '-');
+// 	return element;
+// }
+
+// document.addEventListener('click', () =>{
+// 	getComponent().then(element => {
+// 		document.body.appendChild(element);
+// 	});
+// })
 
 document.addEventListener('click', () =>{
-	getComponent().then(element => {
-		document.body.appendChild(element);
-	});
+	import(/* webpackPrefetch: true */ './click.js').then(({default: func})=>{
+		func()
+	})
 })
