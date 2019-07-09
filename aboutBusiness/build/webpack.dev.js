d54099621ddac0c90da2cf4e701ca6975a50f433
jianghaoran116 <jianghaoran@outlook.com>
2019-07-09 10:59:01 +0800

    eslint

--- aboutBusiness/build/webpack.dev.js
+++ aboutBusiness/build/webpack.dev.js
@@ -4,66 +4,67 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const commonConfig = require('./webpack.common.js');
 
 const devConfig = {
-	mode: 'development',
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: './dist',
-		open: true,
-		port: 8383,
-		hot: true,
-		historyApiFallback: true,
-		// proxy: {
-		// 	'xxx': {
-		// 		'target': 'xxx',
-		// 		'secure': false, //https
-		// 		'pathRewrite': {
-		// 			'xxx': 'xxx'
-		// 		},
-		// 		'changeOrigin': true,
-		// 		'header': {
-		// 			host: '',
-		// 			cookie: ''
-		// 		}
-		// 	}
-		// }
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: {
-							importLoaders: 2
-						}
-					},
-					'sass-loader',
-					'postcss-loader'
-				]
-			}, {
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: true
-						}
-					},
-					'css-loader',
-					'postcss-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	output: {
-		filename: '[name].js',
-		chunkFilename: '[name].chunk.js'
-	}
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    overlay: true,
+    contentBase: './dist',
+    open: true,
+    port: 8383,
+    hot: true,
+    historyApiFallback: true,
+    // proxy: {
+    //   'xxx': {
+    //     'target': 'xxx',
+    //     'secure': false, //https
+    //     'pathRewrite': {
+    //       'xxx': 'xxx'
+    //     },
+    //     'changeOrigin': true,
+    //     'header': {
+    //       host: '',
+    //       cookie: ''
+    //     }
+    //   }
+    // }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }, {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true
+            }
+          },
+          'css-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js'
+  }
 }
 
 module.exports = merge(commonConfig, devConfig);
