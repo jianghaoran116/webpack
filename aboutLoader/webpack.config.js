1d864a6cee596b1f935c9308a4f3ec8698d1d8b7
jianghaoran116 <jianghaoran@outlook.com>
2019-07-12 16:17:46 +0800

    simple loader

---
+++ aboutLoader/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.js'
+	},
+	resolveLoader: {
+		modules: ['node_modules', './loaders']
+	},
+	module: {
+		rules: [{
+			test: /\.js/,
+			use: [
+				{
+					loader: 'replaceLoader',
+				},
+				{
+					loader: 'replaceLoaderAsync',
+					options: {
+						name: '233'
+					}
+				},
+			]
+		}]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'
+	}
+}
